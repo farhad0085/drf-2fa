80c45484cc0adf31260d2bb3f25bdd557f844bad
Farhad Hossain <farhadhossain0085@gmail.com>
2024-05-26 02:46:10 +0600

    updated OTP Backends

--- drf_2fa/backends.py
+++ drf_2fa/backends.py
@@ -7,36 +7,47 @@ from django.template.loader import render_to_string
 
 class BaseOTPBackend:
 
+    def generate_otp(self):
+        self.otp_code = ''.join(random.choices(string.digits, k=drf_2fa_settings.OTP_LENGTH))
+        return self.otp_code
+
     def verify_otp(self, user, otp):
         raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `verify_otp` method")
     
-    def generate_otp(self, user):
-        """This method generates otp and store in database if required"""
-        raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `generate_otp` method")
+    def save_otp(self, user):
+        """store otp in database if required"""
+        raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `save_otp` method")
     
     def send_otp(self, user):
         """This method is responsible to send the otp to user"""
         raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `send_otp` method")
 
-    def generate_and_send_code(self, user):
-        raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `generate_and_send_code` method")
+    def save_and_send_otp_code(self, user):
+        raise NotImplementedError(f"`{self.__class__.__name__}` backend must override `save_and_send_otp_code` method")
 
 
-class EmailOTPBackend(BaseOTPBackend):
+class BaseMessageOTPBackend(BaseOTPBackend):
 
     def verify_otp(self, user, otp):
         is_valid, otp_obj = OTPCode.is_valid(user, otp)
         if is_valid:
             otp_obj.delete() # delete otp from the database, since it's already used and verified
-        
         return is_valid
 
-    def generate_otp(self, user):
-        self.otp_code = ''.join(random.choices(string.digits, k=drf_2fa_settings.OTP_LENGTH))
+    def save_otp(self, user):
+        otp_code = self.generate_otp()
         # save the otp to database
-        OTPCode.objects.create(user=user, otp_code=self.otp_code)
+        OTPCode.objects.create(user=user, otp_code=otp_code)
         return self.otp_code
 
+    def save_and_send_otp_code(self, user):
+        self.save_otp(user)
+        self.send_otp(user)
+
+
+class EmailOTPBackend(BaseMessageOTPBackend):
+    """Send OTP in user's email"""
+    
     def send_otp(self, user):
         subject = render_to_string('drf_2fa/email/subject.txt')
         email_content = render_to_string('drf_2fa/email/message.html', {'otp_code': self.otp_code})
@@ -47,7 +58,3 @@ class EmailOTPBackend(BaseOTPBackend):
             from_email=drf_2fa_settings.OTP_EMAIL_FROM,
             recipient_list=[user.email]
         )
-
-    def generate_and_send_code(self, user):
-        self.generate_otp(user)
-        self.send_otp(user)
--- drf_2fa/signals.py
+++ drf_2fa/signals.py
@@ -1,4 +1,5 @@
 from django.dispatch import receiver, Signal
+from drf_2fa.backends import BaseOTPBackend
 from drf_2fa.settings import drf_2fa_settings
 
 
@@ -8,5 +9,5 @@ otp_required_signal = Signal(["user"])
 @receiver(otp_required_signal)
 def handle_otp_required_signal(user, *args, **kwargs):
     # create otp in database using backend class
-    backend = drf_2fa_settings.DEFAULT_OTP_BACKEND()
-    backend.generate_and_send_code(user)
+    backend: BaseOTPBackend = drf_2fa_settings.DEFAULT_OTP_BACKEND()
+    backend.save_and_send_otp_code(user)
