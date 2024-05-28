6b0483fe57d64652e74fa042fce301c4780a6f93
Farhad Hossain <farhadhossain0085@gmail.com>
2024-05-29 01:54:39 +0600

    docs files updated

--- docs/conf.py
+++ docs/conf.py
@@ -4,17 +4,22 @@
 project = 'DRF 2Factor Authentication'
 copyright = '2024, Farhad Hossain'
 author = 'Farhad Hossain'
+version = "1.0"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
+templates_path = []
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+html_static_path = []
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
