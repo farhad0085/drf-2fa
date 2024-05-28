953dcae51b3a63b9a8804c6061b57d337f90b4de
Farhad Hossain <farhadhossain0085@gmail.com>
2024-05-29 00:55:17 +0600

    demo conf.py added

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DRF 2Factor Authentication'
+copyright = '2024, Farhad Hossain'
+author = 'Farhad Hossain'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
