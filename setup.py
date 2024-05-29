59b2ab7ac2d4830f3afd1c48b686b3cd52fa3f8b
Farhad Hossain <farhadhossain0085@gmail.com>
2024-05-29 15:17:34 +0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (BASE_PATH / "README.md").read_text()
 setup(
     name="drf-2fa",
     version="1.0.0",
-    description="Integrate 2 factor authentication in your django rest api easily",
+    description="Integrate 2 Factor Authentication in Your Django REST API project easily.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/farhad0085/drf-2fa",
@@ -23,9 +23,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["drf_2fa", "drf_2fa/migrations", "drf_2fa/templates", "drf_2fa/backends"],
+    packages=["drf_2fa", "drf_2fa/migrations", "drf_2fa/backends"],
+    package_data={'drf_2fa': ['templates/**/**/*.*']},
     include_package_data=True,
-    install_requires=["django>=2.2"],
+    install_requires=[],
 )
 
 # build
